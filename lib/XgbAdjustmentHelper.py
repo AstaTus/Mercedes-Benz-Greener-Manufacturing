55f6dcaa69f3f4fb2cd9f32ec786ef00796a7033
AstaTus <523182099@qq.com>
2017-06-05 23:47:42 +0800

    pca & ica

---
+++ lib/XgbAdjustmentHelper.py
@@ -0,0 +1,49 @@
+import xgboost as xgb
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.grid_search import GridSearchCV
+
+def modelfit(alg, feature_names, X_train, y_train, X_test, y_test, useTrainCV=True, cv_folds=5,
+             early_stopping_rounds=50):
+    if useTrainCV:
+        xgb_param = alg.get_xgb_params()
+        xgtrain = xgb.DMatrix(X_train, label=y_train)
+        cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], nfold=cv_folds,
+                          metrics='rmse', early_stopping_rounds=early_stopping_rounds)
+        alg.set_params(n_estimators=cvresult.shape[0])
+
+    # Fit the algorithm on the data
+    alg.fit(X_train, y_train)
+
+    # Predict training set:
+    y_predictions = alg.predict(X_test)
+    #     dtrain_predprob = alg.predict_proba(y_test)
+
+    # Print model report:
+    print("\nModel Report")
+    print("RMSE : %.4g" % metrics.mean_squared_error(y_test, y_predictions))
+    # print("AUC Score (Train): %f" % metrics.roc_auc_score(dtrain['Disbursed'], dtrain_predprob))
+
+    feat_imp = pd.Series(alg.booster().get_fscore()).sort_values(ascending=False)
+    feat_imp = feat_imp.rename(lambda x: feature_names[int(x[1:len(x)])])
+
+    feat_imp.plot(kind='barh', title='Feature Importances', figsize=(7, 16))
+    plt.ylabel('Feature Importance Score')
+
+
+def ModelParamSearch(xgb, params, X_train, y_train):
+    search = GridSearchCV(estimator=xgb, param_grid=params, n_jobs=4, iid=False, cv=5)
+    search.fit(X_train, y_train)
+    print('\ngrid_scores')
+    for score in search.grid_scores_:
+        print(score)
+    print('\nbest_params')
+    print(search.best_params_)
+    print('\nbest_score')
+    print(search.best_score_)
+    return search
