b545c892e3923f2213d43d105edb4dc9241ee4a5
junqi chen <523182099@qq.com>
2017-06-12 20:42:32 +0800

    ensemblemodel

---
+++ lib/CVHelper.py
@@ -0,0 +1,58 @@
+# auther = 'chenjunqi'
+# -*- coding: utf-8 -*-
+
+from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import KFold
+import numpy as np
+
+class CVHelper:
+    # def __init__(self):
+
+    @staticmethod
+    def KFoldCV(model, num_fold, X_train, y_train, eval_func, random_state = 100):
+        cv_results = []
+        kf = KFold(n_splits=num_fold, random_state=random_state)
+        for train_index, test_index in kf.split(X_train):
+            X_sub_train = X_train[train_index]
+            X_sub_test = X_train[test_index]
+            y_sub_train = y_train[train_index]
+            y_sub_test = y_train[test_index]
+
+            model.fit(X_sub_train, y_sub_train)
+            y_pred = model.predict(X_sub_test)
+
+            eval = eval_func(y_sub_test, y_pred)
+            cv_results.append(eval)
+            eval_mean =  np.std(cv_results)
+            eval_std = np.std(cv_results)
+            print('eval mean:%f eval std:%f'%(eval_mean, eval_std))
+
+        return eval_mean, eval_std
+
+
+    @staticmethod
+    def LooCV(model, X_train, y_train, eval_func, silent=False, step=100):
+        y_tests = []
+        y_preds = []
+        i = 0
+        loo = LeaveOneOut()
+        for train_index, test_index in  loo.split(X_train):
+            X_sub_train = X_train[train_index]
+            X_sub_test = X_train[test_index]
+            y_sub_train = y_train[train_index]
+            y_sub_test = y_train[test_index]
+
+            model.fit(X_sub_train, y_sub_train)
+            y_pred = model.predict(X_sub_test)
+
+            y_tests.extend(y_sub_test)
+            y_preds.extend(y_pred)
+
+            if silent == False:
+                i = i + 1;
+                if i % step == 0:
+                    print(i)
+
+        eval = eval_func(y_sub_test, y_pred)
+        print('eval:%f'%(eval))
+        return eval
---
+++ lib/Ensembler.py
@@ -0,0 +1,49 @@
+# auther = 'chenjunqi'
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from sklearn.model_selection import KFold
+
+class Ensembler:
+    def __init__(self, n_folds, base_models):
+        self.n_folds = n_folds
+        self.base_models = base_models
+        self.level_one_train = np.zeros();
+        self.level_one_test = np.zeros();
+        self.X = np.array()
+        self.y = np.array()
+        self.T = np.array()
+
+
+    def fit_predict(self, X, y, T):
+        self.X = np.array(X)
+        self.y = np.array(y)
+        self.T = np.array(T)
+
+        folds = list(KFold(len(y), n_folds=self.n_folds, shuffle=True, random_state=2016))
+
+        self.level_one_train = np.zeros((X.shape[0], len(self.base_models)))
+        self.level_one_test = np.zeros((T.shape[0], len(self.base_models)))
+
+        for i, model in enumerate(self.base_models):
+            S_test_i = np.zeros((T.shape[0], len(folds)))
+
+            for j, (train_idx, test_idx) in enumerate(folds):
+                X_train = X[train_idx]
+                y_train = y[train_idx]
+                X_test = X[test_idx]
+                # y_holdout = y[test_idx]
+                model.fit(X_train, y_train)
+                y_pred = model.predict(X_test)[:]
+                self.level_one_train[test_idx, i] = y_pred
+                S_test_i[:, j] = model.predict(T)[:]
+
+            self.level_one_test[:, i] = S_test_i.mean(1)
+
+            return self.level_one_train, self.level_one_test
+
+
+    def stacking(self, stacker_model):
+        stacker_model.fit(self.level_one_train, self.y)
+        y_pred = stacker_model.predict(self.level_one_test)[:]
+        return y_pred
